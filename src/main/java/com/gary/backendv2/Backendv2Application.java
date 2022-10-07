43936f3cbdd6ac35941d280f3000ae7456d3c4fe
bfs <barteksemeniuk57@gmail.com>
2022-10-07 04:15:30 +0200

    initial commit

---
+++ src/main/java/com/gary/backendv2/Backendv2Application.java
@@ -0,0 +1,13 @@
+package com.gary.backendv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backendv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gary/backendv2/Backendv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.backendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
