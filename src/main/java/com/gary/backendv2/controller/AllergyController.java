2480de0a2605efd082bcdb99dedfd85fc7146ed3
Aleksandra Mira <s20383@pjwstk.edu.pl>
2022-10-11 23:03:15 +0200

    Update AllergyController.java

--- src/main/java/com/gary/backendv2/controller/AllergyController.java
+++ src/main/java/com/gary/backendv2/controller/AllergyController.java
@@ -4,6 +4,7 @@ import com.gary.backendv2.model.Allergy;
 import com.gary.backendv2.service.AllergyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,7 @@ public class AllergyController {
 	public List<Allergy> getAllAllergies(){
 		return allergyService.getAll();
 	}
+
+	@GetMapping("/{id}")
+	public Allergy getAllergyById(@PathVariable Integer id){return allergyService.getById(id);}
 }
