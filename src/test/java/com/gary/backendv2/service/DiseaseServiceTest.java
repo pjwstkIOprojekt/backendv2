2fa76fba8db2c29572b70398a4337b4258582b96
Aleksandra Mira <s20383@pjwstk.edu.pl>
2022-10-18 17:22:01 +0200

    Add test find by id disease

--- src/test/java/com/gary/backendv2/service/DiseaseServiceTest.java
+++ src/test/java/com/gary/backendv2/service/DiseaseServiceTest.java
@@ -2,14 +2,18 @@ package com.gary.backendv2.service;
 
 import com.gary.backendv2.model.Allergy;
 import com.gary.backendv2.model.Disease;
+import com.gary.backendv2.model.dto.response.AllergyResponse;
+import com.gary.backendv2.model.dto.response.DiseaseResponse;
 import com.gary.backendv2.repository.DiseaseRepository;
 import com.gary.backendv2.repository.MedicalInfoRepository;
 import com.gary.backendv2.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,4 +33,22 @@ public class DiseaseServiceTest {
 
 		assertEquals(expected.size(), result.size());
 	}
+
+	@Test()
+	void getByIdShouldFind() {
+		int id = 1;
+		Disease expected = new Disease();
+		expected.setDiseaseId(1);
+		expected.setDiseaseName("test");
+
+		when(diseaseRepository.findByDiseaseId(id)).thenReturn(expected);
+
+		DiseaseResponse result = diseaseService.getAllById(1);
+
+		assertNotNull(result);
+		assertEquals(expected.getDiseaseId(), result.getDiseaseId());
+		assertEquals(expected.getDiseaseName(), result.getDiseaseName());
+
+	}
+
 }
