36b1d5e33a6e4d63c8ffef9d16ea1fed1bc0b792
Aleksandra Mira <s20383@pjwstk.edu.pl>
2022-10-23 18:05:11 +0200

    Get all trusted person

--- src/main/java/com/gary/backendv2/controller/TrustedPersonController.java
+++ src/main/java/com/gary/backendv2/controller/TrustedPersonController.java
@@ -2,6 +2,8 @@ package com.gary.backendv2.controller;
 
 import com.gary.backendv2.service.TrustedPersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TrustedPersonController {
 	private final TrustedPersonService trustedPersonService;
+
+	@GetMapping("")
+	public ResponseEntity<?> getAll(){
+		return ResponseEntity.ok(trustedPersonService.getAll());
+	}
 }
--- src/main/java/com/gary/backendv2/service/TrustedPersonService.java
+++ src/main/java/com/gary/backendv2/service/TrustedPersonService.java
@@ -1,11 +1,34 @@
 package com.gary.backendv2.service;
 
+import com.gary.backendv2.model.TrustedPerson;
+import com.gary.backendv2.model.dto.response.TrustedPersonResponse;
 import com.gary.backendv2.repository.TrustedPersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TrustedPersonService {
 	private final TrustedPersonRepository trustedPersonRepository;
+
+	public List<TrustedPersonResponse> getAll(){
+		List<TrustedPerson> trustedPeople = trustedPersonRepository.findAll();
+		List<TrustedPersonResponse> trustedPersonResponses = new ArrayList<>();
+		for (TrustedPerson t: trustedPeople) {
+			trustedPersonResponses.add(
+				TrustedPersonResponse
+						.builder()
+						.email(t.getEmail())
+						.firstName(t.getFirstName())
+						.trustedId(t.getTrustedId())
+						.lastName(t.getLastName())
+						.phone(t.getPhone())
+						.build()
+			);
+		}
+		return trustedPersonResponses;
+	}
 }
