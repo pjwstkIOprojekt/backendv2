0fd7110aea8d2d286249e295133ab0e9251d50d6
Aleksandra Mira <s20383@pjwstk.edu.pl>
2022-10-23 18:31:50 +0200

    Add rest apis for trusted person

--- src/main/java/com/gary/backendv2/controller/TrustedPersonController.java
+++ src/main/java/com/gary/backendv2/controller/TrustedPersonController.java
@@ -1,11 +1,13 @@
 package com.gary.backendv2.controller;
 
+import com.gary.backendv2.model.dto.request.TrustedPersonRequest;
 import com.gary.backendv2.service.TrustedPersonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
 @RestController()
 @RequestMapping(path = "/trusted")
@@ -16,5 +18,28 @@ public class TrustedPersonController {
 	@GetMapping("")
 	public ResponseEntity<?> getAll(){
 		return ResponseEntity.ok(trustedPersonService.getAll());
+	}
+
+	@GetMapping("/{email}")
+	public ResponseEntity<?> getByEmail(@PathVariable @Email String email){
+		return ResponseEntity.ok(trustedPersonService.getByEmail(email));
+	}
+
+	@DeleteMapping("/{email}")
+	public ResponseEntity<?> deleteByEmail(@PathVariable @Email String email){
+		trustedPersonService.deleteByEmail(email);
+		return ResponseEntity.ok("Trusted person deleted successfully.");
+	}
+
+	@PostMapping("")
+	public ResponseEntity<?> addTrustedPerson(@RequestBody @Valid TrustedPersonRequest trustedPersonRequest){
+		trustedPersonService.addTrustedPerson(trustedPersonRequest);
+		return ResponseEntity.ok("Trusted person added successfully");
+	}
+
+	@PutMapping("")
+	public ResponseEntity<?> updateTrustedPerson(@RequestBody @Valid TrustedPersonRequest trustedPersonRequest){
+		trustedPersonService.updateTrustedPerson(trustedPersonRequest);
+		return ResponseEntity.ok("Trusted person updated successfully");
 	}
 }
--- src/main/java/com/gary/backendv2/service/TrustedPersonService.java
+++ src/main/java/com/gary/backendv2/service/TrustedPersonService.java
@@ -1,18 +1,26 @@
 package com.gary.backendv2.service;
 
+import com.gary.backendv2.exception.HttpException;
 import com.gary.backendv2.model.TrustedPerson;
+import com.gary.backendv2.model.User;
+import com.gary.backendv2.model.dto.request.AllergyRequest;
+import com.gary.backendv2.model.dto.request.TrustedPersonRequest;
 import com.gary.backendv2.model.dto.response.TrustedPersonResponse;
 import com.gary.backendv2.repository.TrustedPersonRepository;
+import com.gary.backendv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TrustedPersonService {
 	private final TrustedPersonRepository trustedPersonRepository;
+	private final UserRepository userRepository;
 
 	public List<TrustedPersonResponse> getAll(){
 		List<TrustedPerson> trustedPeople = trustedPersonRepository.findAll();
@@ -30,5 +38,77 @@ public class TrustedPersonService {
 			);
 		}
 		return trustedPersonResponses;
+	}
+
+	public TrustedPersonResponse getByEmail(String userEmail){
+		Optional<User> userOptional = userRepository.findByEmail(userEmail);
+		if (userOptional.isEmpty()) {
+			throw new HttpException(HttpStatus.NOT_FOUND, String.format("Cannot find user with %s", userEmail));
+		}
+		TrustedPerson trustedPerson = userOptional.get().getTrustedPerson();
+		if(trustedPerson == null){
+			throw new HttpException(HttpStatus.NO_CONTENT, String.format("User %s don't have trusted person", userEmail));
+		}
+		return  TrustedPersonResponse
+				.builder()
+				.phone(trustedPerson.getPhone())
+				.email(trustedPerson.getEmail())
+				.lastName(trustedPerson.getLastName())
+				.firstName(trustedPerson.getFirstName())
+				.trustedId(trustedPerson.getTrustedId())
+				.build();
+	}
+
+	public void deleteByEmail(String userEmail){
+		Optional<User> userOptional = userRepository.findByEmail(userEmail);
+		if (userOptional.isEmpty()) {
+			throw new HttpException(HttpStatus.NOT_FOUND, String.format("Cannot find user with %s", userEmail));
+		}
+		TrustedPerson trustedPerson = userOptional.get().getTrustedPerson();
+		if(trustedPerson == null){
+			throw new HttpException(HttpStatus.NO_CONTENT, String.format("User %s don't have trusted person", userEmail));
+		}
+		trustedPersonRepository.delete(trustedPerson);
+		User user = userOptional.get();
+		user.setTrustedPerson(null);
+		userRepository.save(user);
+	}
+
+	public void addTrustedPerson(TrustedPersonRequest trustedPersonRequest){
+		Optional<User> userOptional = userRepository.findByEmail(trustedPersonRequest.getUserEmail());
+		if (userOptional.isEmpty()) {
+			throw new HttpException(HttpStatus.NOT_FOUND, String.format("Cannot find user with %s", trustedPersonRequest.getUserEmail()));
+		}
+		User user = userOptional.get();
+		if(user.getTrustedPerson() != null){
+			throw new HttpException(HttpStatus.BAD_REQUEST, String.format("%s already have trusted person", trustedPersonRequest.getUserEmail()));
+		}
+		TrustedPerson trustedPerson = TrustedPerson.builder()
+				.firstName(trustedPersonRequest.getFirstName())
+				.lastName(trustedPersonRequest.getLastName())
+				.email(trustedPersonRequest.getEmail())
+				.phone(trustedPersonRequest.getPhone())
+				.user(user)
+				.build();
+
+		user.setTrustedPerson(trustedPerson);
+		trustedPersonRepository.save(trustedPerson);
+		userRepository.save(user);
+	}
+
+	public void updateTrustedPerson(TrustedPersonRequest trustedPersonRequest){
+		Optional<User> userOptional = userRepository.findByEmail(trustedPersonRequest.getUserEmail());
+		if (userOptional.isEmpty()) {
+			throw new HttpException(HttpStatus.NOT_FOUND, String.format("Cannot find user with %s", trustedPersonRequest.getUserEmail()));
+		}
+		TrustedPerson trustedPerson = userOptional.get().getTrustedPerson();
+		if(trustedPerson == null){
+			throw new HttpException(HttpStatus.NO_CONTENT, String.format("User %s don't have trusted person", trustedPersonRequest.getUserEmail()));
+		}
+		trustedPerson.setEmail(trustedPersonRequest.getEmail());
+		trustedPerson.setFirstName(trustedPersonRequest.getFirstName());
+		trustedPerson.setLastName(trustedPersonRequest.getLastName());
+		trustedPerson.setPhone(trustedPersonRequest.getPhone());
+		trustedPersonRepository.save(trustedPerson);
 	}
 }
