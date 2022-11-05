dc77d6c1765627ae2a787a8353a55e562f8be0ca
Aleksandra Mira <s20383@pjwstk.edu.pl>
2022-11-05 13:51:44 +0100

    forgotten controller

--- src/main/java/com/gary/backendv2/controller/FacilityController.java
+++ src/main/java/com/gary/backendv2/controller/FacilityController.java
@@ -1,11 +1,12 @@
 package com.gary.backendv2.controller;
 
+import com.gary.backendv2.model.dto.request.FacilityRequest;
 import com.gary.backendv2.service.FacilityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +17,28 @@ public class FacilityController {
 	@GetMapping
 	public ResponseEntity<?> getAll(){
 		return ResponseEntity.ok(facilityService.getAll());
+	}
+
+	@GetMapping("/id")
+	public ResponseEntity<?> getById(@PathVariable Integer id){
+		return ResponseEntity.ok(facilityService.getById(id));
+	}
+
+	@DeleteMapping("/id")
+	public ResponseEntity<?> delete(@PathVariable Integer id){
+		facilityService.delete(id);
+		return ResponseEntity.ok("Facility successfully deleted");
+	}
+
+	@PostMapping
+	public ResponseEntity<?> add(@RequestBody @Valid FacilityRequest facilityRequest){
+		facilityService.add(facilityRequest);
+		return ResponseEntity.ok("Facility successfully added");
+	}
+
+	@PutMapping("/id")
+	public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody @Valid FacilityRequest facilityRequest){
+		facilityService.update(id,facilityRequest);
+		return ResponseEntity.ok("Facility updated successfully");
 	}
 }
