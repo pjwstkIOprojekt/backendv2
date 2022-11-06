15ff67195cfdb58ae70151fed34a4206d4d2ceae
S21227 <stasgodwod@gmail.com>
2022-11-06 16:47:39 +0100

    Aded unit tests for ambulance service

--- src/main/java/com/gary/backendv2/repository/TrustedPersonRepository.java
+++ src/main/java/com/gary/backendv2/repository/TrustedPersonRepository.java
@@ -1,7 +1,11 @@
 package com.gary.backendv2.repository;
 
+import com.gary.backendv2.model.Ambulance;
 import com.gary.backendv2.model.TrustedPerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrustedPersonRepository extends JpaRepository<TrustedPerson, Integer> {
+    Optional<TrustedPerson> findByEmail(String licensePlate);
 }
