3bd778339cd2f6e615bcd0b8c04e8f56d1a94e65
Bartosz Semeniuk <s20462@pjwstk.edu.pl>
2022-12-15 01:23:10 +0100

    schedule related reworks and fixes (#63)

--- src/main/java/com/gary/backendv2/model/dto/response/WorkScheduleResponse.java
+++ src/main/java/com/gary/backendv2/model/dto/response/WorkScheduleResponse.java
@@ -3,7 +3,7 @@ package com.gary.backendv2.model.dto.response;
 import com.gary.backendv2.model.dto.request.users.RegisterEmployeeRequest;
 import lombok.*;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Getter
@@ -11,5 +11,5 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class WorkScheduleResponse {
-    Map<String, RegisterEmployeeRequest.ScheduleDto> newSchedule = new HashMap<>();
+    Map<String, RegisterEmployeeRequest.ScheduleDto> schedule = new LinkedHashMap<>();
 }
